069625446cef29dd17fad3d3ed7a805c5cd09f39
Gabriel Dominguez <gabriel@gabrieldominguez.es>
2026-06-18 16:42:01 +0200

    Test sellAsset wok

--- tests/test_sell_asset.py
+++ tests/test_sell_asset.py
@@ -1,6 +1,6 @@
 import os
 import requests
-import pytest
+import json
 
 API_ENDPOINT = os.getenv("API_ENDPOINT")
 API_KEY = os.getenv("API_KEY")
@@ -8,6 +8,7 @@ API_KEY = os.getenv("API_KEY")
 URL = f"https://{API_ENDPOINT}/sellAsset?key={API_KEY}"
 
 def test_add_asset():
+    expected_result= {"ops": [{"buy_price_eur": 31.0, "earnings": 38.500000000000014, "retired": 5.0, "sold_price_eur": 38.7}, {"buy_price_eur": 30.0, "earnings": 125.50000000000004, "retired": 10.0, "sold_price_eur": 38.7}, {"buy_price_eur": 28.38, "earnings": 135.82000000000005, "retired": 1.0, "sold_price_eur": 38.7}], "total_available": 25.0, "total_available_after": 9.0, "total_earnings": 135.82000000000005, "total_retired": 16.0}
     resp = requests.post(URL, json={
         "origin" : {
             "wallet" : 88,
@@ -28,6 +29,7 @@ def test_add_asset():
         "datetime" : "2026-06-16 17:19:16",
         "comments" : ""
     })
-    print(resp)
+    
     assert resp.status_code == 200
+    assert resp.json() == expected_result
 
