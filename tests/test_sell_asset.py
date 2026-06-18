5eb25e1b8a42d436fbf1176d5e4b2f0b740b4d1d
Gabriel Dominguez <gabriel@gabrieldominguez.es>
2026-06-18 16:32:56 +0200

    Change url sellAsset

--- tests/test_sell_asset.py
+++ tests/test_sell_asset.py
@@ -5,7 +5,7 @@ import pytest
 API_ENDPOINT = os.getenv("API_ENDPOINT")
 API_KEY = os.getenv("API_KEY")
 
-URL = f"https://{API_ENDPOINT}/assets?key={API_KEY}"
+URL = f"https://{API_ENDPOINT}/sellAsset?key={API_KEY}"
 
 def test_add_asset():
     resp = requests.post(URL, json={
