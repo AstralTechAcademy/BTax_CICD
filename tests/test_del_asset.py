c37e357445e9944f5d28eb075f965e26bf66e632
Gabriel Dominguez <gabriel@gabrieldominguez.es>
2026-06-23 14:33:47 +0200

    Change function name

--- tests/test_del_asset.py
+++ tests/test_del_asset.py
@@ -7,7 +7,7 @@ API_KEY = os.getenv("API_KEY")
 
 URL = f"https://{API_ENDPOINT}/assets"
 
-def main():
+def test_del_asset():
     resp = requests.delete(URL, params={"key": API_KEY, "name": "TEST1"})
     assert resp.status_code == 200
 
